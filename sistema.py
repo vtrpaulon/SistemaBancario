dc1c5a10b95f18ba280f47e2ae9705a5b4382d30
Vitor <vitorpaulon@gmail.com>
2024-06-26 20:44:51 -0300

    update 26-06-24

--- sistema.py
+++ sistema.py
@@ -19,7 +19,7 @@ def depositar(saldo, valor, extrato, /):
         extrato += f"Depósito:\tR$ {valor:.2f}\n"
         print("\n=== Depósito realizado com sucesso! ===")
     else:
-        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        print("\n Operação falhou! O valor informado é inválido. ")
 
     return saldo, extrato
 
@@ -29,18 +29,18 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
     excedeu_saques = numero_saques >= limite_saques
 
     if excedeu_saldo:
-        print("\n@@@ Operação falhou! Você não tem saldo suficiente. @@@")
+        print("\n Operação falhou! Você não tem saldo suficiente. ")
     elif excedeu_limite:
-        print("\n@@@ Operação falhou! O valor do saque excede o limite. @@@")
+        print("\n Operação falhou! O valor do saque excede o limite. ")
     elif excedeu_saques:
-        print("\n@@@ Operação falhou! Número máximo de saques excedido. @@@")
+        print("\n Operação falhou! Número máximo de saques excedido. ")
     elif valor > 0:
         saldo -= valor
         extrato += f"Saque:\t\tR$ {valor:.2f}\n"
         numero_saques += 1
-        print("\n=== Saque realizado com sucesso! ===")
+        print("\n Saque realizado com sucesso! ")
     else:
-        print("\n@@@ Operação falhou! O valor informado é inválido. @@@")
+        print("\n Operação falhou! O valor informado é inválido. ")
 
     return saldo, extrato, numero_saques
 
@@ -55,7 +55,7 @@ def criar_usuario(usuarios):
     usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
-        print("\n@@@ Já existe usuário com esse CPF! @@@")
+        print("\n Já existe usuário com esse CPF! ")
         return
 
     nome = input("Informe o nome completo: ")
@@ -64,7 +64,7 @@ def criar_usuario(usuarios):
 
     usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
 
-    print("=== Usuário criado com sucesso! ===")
+    print(" Usuário criado com sucesso! ")
 
 def filtrar_usuario(cpf, usuarios):
     usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
@@ -75,10 +75,10 @@ def criar_conta(agencia, numero_conta, usuarios):
     usuario = filtrar_usuario(cpf, usuarios)
 
     if usuario:
-        print("\n=== Conta criada com sucesso! ===")
+        print("\n Conta criada com sucesso! ")
         return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
 
-    print("\n@@@ Usuário não encontrado, fluxo de criação de conta encerrado! @@@")
+    print("\n Usuário não encontrado, fluxo de criação de conta encerrado! ")
 
 def listar_contas(contas):
     for conta in contas:
